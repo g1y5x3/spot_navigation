87f5a4fc1734b4759ae6c37b7c7985e4bf49fe2f
Yixiang Gao <yg5d6@mail.missouri.edu>
2026-02-06 09:44:01 -0600

    major clean up

--- setup.py
+++ setup.py
@@ -1,36 +1,32 @@
 from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'spot_navigation'
+package_name = "spot_navigation"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
         ("share/" + package_name + "/rviz", glob("rviz/*.rviz")),
         ("share/" + package_name + "/config", glob("config/*.yaml")),
         ("share/" + package_name + "/map", glob("map/*.pcd")),
-
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='spot',
-    maintainer_email='spot@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer="spot",
+    maintainer_email="spot@todo.todo",
+    description="TODO: Package description",
+    license="TODO: License declaration",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': [
-            'initial_map2odom_publisher = spot_navigation.initial_map2odom_publisher:main',
-        ],
+        "console_scripts": [],
     },
 )
